212a570a4bd54e7c03d0fb15159cede80c23cd40
Jonathan Clark <jonclark822@gmail.com>
2020-06-05 09:24:47 -0700

    set up api validation

--- src/app.js
+++ src/app.js
@@ -4,14 +4,55 @@ const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config.js');
+const winston = require('winston');
 
 const app = express();
 
 const morganOption = NODE_ENV === 'production';
 
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  transports: [new winston.transports.File({ filename: info.log })],
+});
+
+if (NODE_ENV !== 'production') {
+  logger.add(
+    new winston.transports.Console({
+      format: winston.format.simple(),
+    })
+  );
+}
+
+const bookmarks = [
+  {
+    id,
+    title,
+    url,
+    rating,
+    desc,
+  },
+  {
+    id: 1,
+    title: 'test bookmark',
+    url: 'https://www.test.com',
+    rating: 3,
+    desc: 'test desc',
+  },
+];
+
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get('Authorization');
+
+  if (!authToken || authToken.split(' ') !== apiToken) {
+    return res.status(401).json({ error: 'Unathorized request' });
+  }
+  next();
+});
 
 app.get('/', (req, res) => {
   res.send('Hello, world!');
